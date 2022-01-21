fe557e1725e20fe6c823d73092e7589f37b69f4f
Rory Glenn <glennrory@gmail.com>
2022-01-21 13:49:35 -0800

    Adding operate() back in along with next()

--- src/strategies/DCA3C/dca3c_strategy.py
+++ src/strategies/DCA3C/dca3c_strategy.py
@@ -66,6 +66,38 @@ class DCA3C(bt.Strategy):
         return
 
 
+    def set_take_profit(self) -> None:
+        '''
+        Function to update the take profit order when new safety orders are placed
+        '''
+
+        if self.take_profit_order is None:
+            return
+
+        if self.take_profit_order.status in [self.take_profit_order.Completed]:
+            return
+
+        # If a sell order has already been made, cancel it
+        self.cancel(self.take_profit_order)
+
+        print()
+        print("CANCELED TAKE PROFIT SELL ORDER")
+        print(f"Price: {self.take_profit_order.price}, Size: {self.take_profit_order.size}")
+
+        total_quantity = self.dca.total_quantities.pop(0)
+        required_price = self.dca.required_price_levels.pop(0)
+
+        self.take_profit_order = self.sell(price=required_price,
+                                           size=total_quantity,
+                                           exectype=bt.Order.Limit)
+
+        print()
+        print("NEW TAKE PROFIT ORDER")
+        print(f"Price: {self.take_profit_order.price}, Size: {self.take_profit_order.size}")
+        print()
+        return
+
+
     def notify_order(self, order: bt.order.BuyOrder):
         # Check if an order has been completed
         # Attention: broker could reject order if not enough cash
@@ -79,22 +111,6 @@ class DCA3C(bt.Strategy):
 
                 # Update current deal variables
                 self.has_position = True
-
-                # if self.has_base_order_filled:
-                #     filled_orders = [_order for _order in self.orders if not _order.alive() and order.exectype != 0]
-                #     total_quantity = 0
-                #     required_price = 0
-
-                #     for _order in filled_orders:
-                #         total_quantity = self.dca.total_quantities.pop(0)
-                #         required_price = self.dca.required_price_levels.pop(0)
-
-                    # self.take_profit_order = self.sell(price=required_price,
-                    #                                    size=total_quantity,
-                    #                                    exectype=bt.Order.Limit)
-
-                # else:
-                #     self.has_base_order_filled = True
 
                 # Update the sell order take profit price every time a new safety order is filled
                 if self.has_base_order_filled:
@@ -128,62 +144,10 @@ class DCA3C(bt.Strategy):
             self.log('OPERATION PROFIT, GROSS %.6f, NET %.6f, Size: %.6f' % (trade.pnl, trade.pnlcomm, trade.size))
         return
 
-    def set_take_profit(self) -> None:
-        '''
-        Function to update the take profit order when new safety orders are placed
-        '''
-
-        if self.take_profit_order is None:
-            return
-
-        if self.take_profit_order.status in [self.take_profit_order.Completed]:
-            return
-
-        # If a sell order has already been made, cancel it
-        self.cancel(self.take_profit_order)
-
-        print()
-        print("CANCELED TAKE PROFIT SELL ORDER")
-        print(f"Price: {self.take_profit_order.price}, Size: {self.take_profit_order.size}")
-
-        total_quantity = self.dca.total_quantities.pop(0)
-        required_price = self.dca.required_price_levels.pop(0)
-
-        self.take_profit_order = self.sell(price=required_price,
-                                           size=total_quantity,
-                                           exectype=bt.Order.Limit)
-
-        print()
-        print("NEW TAKE PROFIT ORDER")
-        print(f"Price: {self.take_profit_order.price}, Size: {self.take_profit_order.size}")
-        print()
-        return
-
-    def next_open(self) -> None:
-        """
-        https://www.backtrader.com/blog/posts/2017-05-01-cheat-on-open/cheat-on-open/
-        If cheat-on-open is True, it will only operate from next_open
-        If cheat-on-open is False, it will only operate from next
-
-        In both cases the matching price must be the same
-        If not cheating, the order is issued at the end of the previous day and will be matched with the next incoming price which is the open price
-        If cheating, the order is issued on the same day it is executed. 
-        Because the order is issued before the broker has evaluated orders, it will also be matched with the next incoming price, the open price.
-        This second scenario, allows calculation of exact stakes for all-in strategies, because one can directly access the current open price.
-
-        In both cases
-        The current open and close prices will be printed from next. 
-        
-        """
-        print("\n-> NEXT_OPEN CANDLE STICK ->")
-        print(f"{self.data.datetime.date()} next open, Open: {self.data.open[0]}, High: {self.data.high[0]}, Low: {self.data.low[0]}, Close: {self.data.close[0]}")
-
-        if not self.cheating:
-            return
-
-        # self.operate(fromopen=True) # why doesn't this work??
-
+    def operate(self, fromopen: bool) -> None:
         if self.position.size == 0:
+            print('{} Send Buy, fromopen {}, close {}'.format(self.data.datetime.date(), fromopen, self.data.close[0]))
+
             print('')
             print('*** NEW DEAL ***')
             entry_price = self.data_open[0]
@@ -215,69 +179,60 @@ class DCA3C(bt.Strategy):
                 buy_order = self.buy(price=self.dca.price_levels[i],
                                      size=self.dca.quantities[i],
                                      exectype=bt.Order.Limit)
-                self.safety_orders.append(buy_order)
+                self.safety_orders.append(buy_order)        
         return
 
-    def nextstart_open(self) -> None:
-        print("\n-> NEXT_START ->")
-        print(f"{self.data.datetime.date()} next start open, Open: {self.data.open[0]}, High: {self.data.high[0]}, Low: {self.data.low[0]}, Close: {self.data.close[0]}")        
-        return 
+
+    def next_open(self) -> None:
+        """
+        https://www.backtrader.com/blog/posts/2017-05-01-cheat-on-open/cheat-on-open/
+        If cheat-on-open is True, it will only operate from next_open
+        If cheat-on-open is False, it will only operate from next
+
+        In both cases the matching price must be the same
+        If not cheating, the order is issued at the end of the previous day and will be matched with the next incoming price which is the open price
+        If cheating, the order is issued on the same day it is executed. 
+        Because the order is issued before the broker has evaluated orders, it will also be matched with the next incoming price, the open price.
+        This second scenario, allows calculation of exact stakes for all-in strategies, because one can directly access the current open price.
+
+        In both cases
+        The current open and close prices will be printed from next. 
+        
+        """
+        print("\n-> NEXT_OPEN CANDLE STICK ->")
+        print(f"{self.data.datetime.date()} next open, Open: {self.data.open[0]}, High: {self.data.high[0]}, Low: {self.data.low[0]}, Close: {self.data.close[0]}")
+
+        if not self.cheating:
+            return
+
+        self.operate(fromopen=True)
+        return
+
+    # def nextstart_open(self) -> None:
+    #     print("\n-> NEXT_START ->")
+    #     print(f"{self.data.datetime.date()} next start open, Open: {self.data.open[0]}, High: {self.data.high[0]}, Low: {self.data.low[0]}, Close: {self.data.close[0]}")        
+    #     return 
 
     def prenext_open(self) -> None:
         print("\n-> PRE_NEXT_OPEN ->")
         print(f"{self.data.datetime.date()} pre next open, Open: {self.data.open[0]}, High: {self.data.high[0]}, Low: {self.data.low[0]}, Close: {self.data.close[0]}")
+
+        # check which orders were filled?
+        for order in self.safety_orders:
+            print(order.price, order.size, order.status, order.alive())
         return
 
-    # def next(self):
-    #     print("\n-> NEXT ->")
-    #     print(f"{self.data.datetime.date()} next, Open: {self.data.open[0]}, High: {self.data.high[0]}, Low: {self.data.low[0]}, Close: {self.data.close[0]}")
 
-    #     # print("\n-> NEXT CANDLE STICK ->")
-    #     # self.log('Open, %.6f' % self.data_close[0]) # this is a rounded number
 
-    #     if self.cheating:
-    #         # this will take us to nextstart_open()
-    #         return
+    def next(self):
+        print("\n-> NEXT ->")
+        print(f"{self.data.datetime.date()} next, Open: {self.data.open[0]}, High: {self.data.high[0]}, Low: {self.data.low[0]}, Close: {self.data.close[0]}")
+
+        if self.cheating:
+            return
         
-    #     # self.operate(fromopen=False)
-
-    #     # If we are not in the market start the next deal
-    #     # if not self.has_position:
-    #     if self.position.size == 0:
-    #         print('')
-    #         print('*** NEW DEAL ***')
-    #         # entry_price = self.price[0]
-    #         entry_price = self.data_open[0]
-
-    #         self.dca = DCA(entry_price,
-    #                        self.params.target_profit_percent,
-    #                        self.params.safety_orders_max,
-    #                        self.params.safety_orders_active_max,
-    #                        self.params.safety_order_volume_scale,
-    #                        self.params.safety_order_step_scale, 
-    #                        self.params.safety_order_price_deviation,
-    #                        self.params.base_order_size, 
-    #                        self.params.safety_order_size)
-    #         self.dca.start()
-    #         # self.dca.print_table()
-
-    #         # BASE ORDER BUY
-    #         buy_order = self.buy(price=entry_price,
-    #                              size=self.params.base_order_size)
-    #         self.safety_orders.append(buy_order)
-
-    #         # BASE ORDER SELL         
-    #         self.take_profit_order = self.sell(price=entry_price + (entry_price * (self.params.target_profit_percent/100)),
-    #                                            size=self.params.base_order_size,
-    #                                            exectype=bt.Order.Limit)
-
-    #         # SAFETY ORDERS
-    #         for i in range(self.dca.safety_orders_active_max):
-    #             buy_order = self.buy(price=self.dca.price_levels[i],
-    #                                  size=self.dca.quantities[i],
-    #                                  exectype=bt.Order.Limit)
-    #             self.safety_orders.append(buy_order)
-    #     return
+        self.operate(fromopen=False)
+        return
 
     def stop(self) -> None:
         print("Finished Backtesting")
